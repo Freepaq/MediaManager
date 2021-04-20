d9417f10cd94e97e1c8af73f605054742a66d0fa
Freepaq <webmaster@freepaq.com>
2021-04-20 22:21:52 +0200

    test

---
+++ cmd/MediaManager_test.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"testing"
+
+	"github.com/Freepaq/MediaManagement/pkg/MediaUtils"
+)
+
+func TestPhoto(t *testing.T) {
+	fname := "testfiles/phototest.jpg"
+	f, err := MediaUtils.GetMeta(fname)
+	if nil != err {
+		t.Failed()
+		t.Errorf(err.Error())
+	}
+	if f.CreationDate.Format("2006-01-02 15:04") != "2021-04-17 17:11" && f.MetaOrigin != MediaUtils.METAORIGINFILE {
+		t.Failed()
+		t.Errorf("Failed to read Meta from photo (" + f.CreationDate.Format("2006-01-02 15:04") + ") " + fname)
+	}
+
+}
+
+func TestVideo(t *testing.T) {
+	fname := "testfiles/videotest.mp4"
+	f, err := MediaUtils.GetMeta(fname)
+	if nil != err {
+		t.Failed()
+		t.Errorf(err.Error())
+	}
+	if f.CreationDate.Format("2006-01-02 15:04") != "2017-07-07 10:16" {
+		t.Failed()
+		t.Errorf("Failed to read Meta from video (" + f.CreationDate.Format("2006-01-02 15:04") + ") " + fname)
+	}
+
+}
--- go.mod
+++ go.mod
@@ -2,4 +2,8 @@ module MediaManager
 
 go 1.16
 
-require github.com/Freepaq/MediaManagement v1.1.1
+require (
+	github.com/Freepaq/MediaManagement v1.1.1
+	github.com/dsoprea/go-exif/v3 v3.0.0-20210131231135-d154f10435cc
+	github.com/dsoprea/go-logging v0.0.0-20200710184922-b02d349568dd
+)
